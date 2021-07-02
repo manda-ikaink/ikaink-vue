b270a44fb501fa4f0b19d6ac5c2a8bb546b6cd11
Amanda Eldreth <manda.eldreth@gmail.com>
2021-07-01 20:22:33 -0700

    Add basic layout and global styles

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="__page">
+    <header></header>
+
+    <main>
+      <Nuxt />
+    </main>
+
+    <footer></footer>
+    
+    <div class="bg-main"></div>
+  </div>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/framework.scss'
+    '~/assets/css/theme.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
