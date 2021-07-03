71cd6013ec34cce96ef67bb9aa7e3ad5186e77ce
Amanda Eldreth <manda.eldreth@gmail.com>
2021-07-03 12:50:59 -0700

    Add footer structure and styles

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,29 @@
 <template>
-  <footer></footer>
+  <footer class="page-footer mt-auto">
+    <div class="container d-flex flex-column flex-lg-row-reverse justify-content-center justify-content-lg-between pt-4">
+      <div class="page-footer__social text-center mb-4">
+        <SocialMedia></SocialMedia>
+      </div>
+      
+      <div class="page-footer__copyright d-lg-flex align-items-lg-center justify-content-center justify-content-lg-end text-center mb-4">
+        <p class="mb-0 ml-lg-2">©{{ year }} {{ $store.state.websiteTitle }} - All rights reserved.</p>
+        <p class="mb-0 ml-lg-2">
+          <NuxtLink to="/privacy-policy">Privacy Policy</NuxtLink>&nbsp;|&nbsp;
+          <NuxtLink to="/terms-of-service">Terms of Service</NuxtLink>&nbsp;|&nbsp;
+          <NuxtLink to="/sitemap">Sitemap</NuxtLink>&nbsp;|&nbsp;
+          <NuxtLink to="/credits">Credits</NuxtLink>
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      year: new Date().getFullYear(),
+    }
+  }
+}
+</script>
