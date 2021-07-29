233564f9a9f9ba11a478e632809c94cf1a3d838a
Amanda Eldreth <manda.eldreth@gmail.com>
2021-07-29 00:54:24 -0700

    Update whitelisted classes for purge

--- nuxt.config.js
+++ nuxt.config.js
@@ -107,7 +107,7 @@ export default {
 
   // PurgeCSS plugin for nuxt: https://purgecss.com/guides/nuxt.html
   purgeCSS: {
-    whitelist: ['body', 'html', 'nuxt-progress', 'show', 'fade', 'active', 'collapse', 'hide', 'close', 'collapse', 'collapsing', 'modal-backdrop'],
+    whitelist: ['body', 'html', 'nuxt-progress', 'show', 'fade', 'active', 'collapse', 'hide', 'close', 'collapse', 'collapsing', 'modal-backdrop', 'field-message', 'form-message', 'invalid-feedback', 'was-validated'],
     whitelistPatterns: [/^ratio/],
   },
 }
