8eab39d5fd2c46c7d8b7557944a03a71b8a5b4c2
Amanda Eldreth <manda.eldreth@gmail.com>
2021-11-04 23:19:20 -0700

    Update default og image settings

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -3,7 +3,7 @@
     <SocialHead
       :title="page.og_title || page.title"
       :description="page.og_description || page.meta_description"
-      :image="page.og_image ? `${$config.apiRoute}/assets/${page.og_image}` : null"
+      :image="page.og_image ? `${$config.apiRoute}/assets/${page.og_image}?fit=cover&width=600&height=300` : `${$config.baseURL}/ikaink.png`"
     />
 
     <PageHeading :title="page.title" :subtitle="page.subtitle">
